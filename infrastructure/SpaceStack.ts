dace290f43773dacfb890b05fd32dcd2edbf39a0
Tauqueer Khan <tauqeer@stackfive.io>
2022-05-15 16:46:52 -0400

    brought in AWS package and queried S3 bucket and added a simple test to run lambda code locally

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -10,6 +10,7 @@ import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 import { GenericTable } from "./GenericTable";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "SpaceApi");
   private spaceTable = new GenericTable("SpacesTable", "spaceId", this);
@@ -17,18 +18,18 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "HelloLambda", {
-      runtime: Runtime.NODEJS_16_X,
-      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-      handler: "hello.main",
-    });
-
     const helloLambadaNodeJs = new NodejsFunction(this, "NodeJsFunction", {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
+    
+    const s3ListPolicy = new PolicyStatement()
+    s3ListPolicy.addActions('s3:ListAllMyBuckets')
+    s3ListPolicy.addResources('*')
+
+    helloLambadaNodeJs.addToRolePolicy(s3ListPolicy)
     // Hello API lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambadaNodeJs);
     const helloLamdaResource = this.api.root.addResource("hello");
     helloLamdaResource.addMethod("GET", helloLambdaIntegration);
   }
